303b2c237fec2211324572e5236885ef69142c5d
anurag91 <anuragbhardwajofficial91@gmail.com>
2020-06-25 01:08:13 +0530

    Update executeForLogs.sh

--- executeForLogs.sh
+++ executeForLogs.sh
@@ -1,6 +1,7 @@
 #git log --oneline --decgit log --oorate > log.log
 #ls -ltr  > log.log
 git log --pretty=format:"%h - %an, %ar : %s" > log.log
+pwd >> log.log
 git diff >> log.log
 git diff HEAD >> log.log
 git diff --cached >> log.log
