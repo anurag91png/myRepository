893b09f05492d5a5caa282fb6440ad514f0eb6c1
anurag91 <anuragbhardwajofficial91@gmail.com>
2020-06-25 16:43:54 +0530

    Update executeForLogs.sh

--- executeForLogs.sh
+++ executeForLogs.sh
@@ -1,8 +1,12 @@
 #git log --oneline --decgit log --oorate > log.log
 #ls -ltr  > log.log
-git log --pretty=format:"%h - %an, %ar : %s" > log.log
-pwd >> log.log
-git diff >> log.log
-git diff HEAD >> log.log
-git diff --cached >> log.log
-$ find "*.txt" -type f -exec echo file {}; >> log.log
+#git log --pretty=format:"%h - %an, %ar : %s" > log.log
+#pwd >> log.log
+#git diff >> log.log
+#git diff HEAD >> log.log
+#git diff --cached >> log.log
+#$ find "*.txt" -type f -exec echo file {}; >> log.log
+for file in *
+do
+   echo $file > log.log
+done
