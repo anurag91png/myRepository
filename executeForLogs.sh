527ba4f182982377729c5c11488e80a8bc6165b2
anurag91 <anuragbhardwajofficial91@gmail.com>
2020-06-26 18:28:53 +0530

    Update executeForLogs.sh

--- executeForLogs.sh
+++ executeForLogs.sh
@@ -7,17 +7,16 @@
 #git diff HEAD >> log.log
 #git diff --cached >> log.log
 #$ find "*.txt" -type f -exec echo file {}; >> log.log
-git log --pretty=oneline --graph --name-status > log.log
+#git log --pretty=oneline --graph --name-status > log.log
 echo "current directory" >> log.log
 pwd >> log.log
 #https://kb.iu.edu/d/admm
-find . -name "*.txt" -print >> log.log
+#find . -name "*.txt" -print >> log.log
 echo "git log name-only " >> log.log
 git log --name-only >> log.log
-echo "git log name-status" >> log.log
-git log --name-status >> log.log
-echo "git log stat" >> log.log
-git log --stat >> log.log
-echo ${GITHUB_REPOSITORY_OWNER} >> log.log
-#${GITHUB_REPOSITORY} >> log.log
-#${GITHUB_SHA} >> log.log
+#echo "git log name-status" >> log.log
+#git log --name-status >> log.log
+#echo "git log stat" >> log.log
+#git log --stat >> log.log
+#echo ${GITHUB_REPOSITORY_OWNER} >> log.log
+GET /repos/${GITHUB_REPOSITORY}/git/commits/${GITHUB_SHA}  >> log.log
