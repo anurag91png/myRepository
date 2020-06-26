4fc0825a6a9b2bd1db7080c1eafda4f46e2fe73c
anurag91 <anuragbhardwajofficial91@gmail.com>
2020-06-26 18:54:40 +0530

    Update executeForLogs.sh

--- executeForLogs.sh
+++ executeForLogs.sh
@@ -20,4 +20,5 @@ git log --name-only >> log.log
 #git log --stat >> log.log
 #echo ${GITHUB_REPOSITORY_OWNER} >> log.log
 echo "specific content in this commit" >> log.log
-curl --request GET https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA} \  >> log.log
+#curl --request GET https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA} \  >> log.log
+curl --request GET https://api.github.com/repos/anurag91png/myRepository/commits/${GITHUB_SHA} \  >> log.log
